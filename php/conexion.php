263f8e0d71ae45ac1ac4d8d816a4e1805f14f19f
Salvador2198 <40402224+Salvador2198@users.noreply.github.com>
2021-06-07 10:10:44 -0500

    Update conexion.php
    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'deporte');
+define('DB_HOST', '138.128.165.82');
+define('DB_USER', 'ticien_deportes');
+define('DB_PASS', '5Hj[Z6DS$*)I');
+define('DB_NAME', 'ticien_deportes');
 
 $conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
