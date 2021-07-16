a84ce81f08a015bb189efb8a0aeb3eef1204c7cd
Salvadord21 <69486356+Salvadord21@users.noreply.github.com>
2021-07-16 16:14:17 -0500

    SELECT

--- admin/imc/select-torneo.php
+++ admin/imc/select-torneo.php
@@ -4,29 +4,29 @@ require '../php/conexion.php';
 $fbardas="SELECT MAX(`id`) as id FROM creacion_torneo WHERE disciplina='futbol bardas'";
 $resultadoB = mysqli_query($conexion, $fbardas);
 $mostrarB=mysqli_fetch_array($resultadoB);
-$idBaas2=$mostrarB['id'];
+$idBaas3=$mostrarB['id'];
 
 $fascenso="SELECT MAX(`id`) as id2 FROM creacion_torneo WHERE disciplina='ascenso'";
 $resultadoA = mysqli_query($conexion, $fascenso);
 $mostrarA=mysqli_fetch_array($resultadoA);
-$idAs2 = $mostrarA['id2'];
+$idAs3 = $mostrarA['id2'];
 
 $fifa="SELECT MAX(`id`) as id FROM creacion_torneo WHERE disciplina='fifa'";
 $resultadoF = mysqli_query($conexion, $fifa);
 $mostrarF=mysqli_fetch_array($resultadoF);
-$idFif2= $mostrarF['id'];
+$idFif3= $mostrarF['id'];
 
 $voley="SELECT MAX(`id`) as id FROM creacion_torneo WHERE disciplina='voleibol'";
 $resultadoV = mysqli_query($conexion, $voley);
 $mostrarV=mysqli_fetch_array($resultadoV);
-$idVol2 = $mostrarV['id'];
+$idVol3 = $mostrarV['id'];
 
 $basquet="SELECT MAX(`id`) as id FROM creacion_torneo WHERE disciplina='Basquetbol'";
 $resultadoBa = mysqli_query($conexion, $basquet);
 $mostrarBa=mysqli_fetch_array($resultadoBa);
-$idBasquet2 = $mostrarBa['id'];
+$idBasquet3 = $mostrarBa['id'];
 
-$torneosregis = "SELECT * FROM creacion_torneo WHERE `id`='$idBaas2' or `id`='$idAs2' or `id`='$idBasquet2' or `id`='$idFif2' or `id`='$idVol2'";
+$torneosregis = "SELECT * FROM creacion_torneo WHERE `delete` is NULL and ( `id`='$idBaas3' or `id`='$idAs3' or `id`='$idBasquet3' or `id`='$idFif3' or `id`='$idVol3')";
 
 $query = mysqli_query($conexion, $torneosregis);
 
